3e204a9dea16a4f4bd986b21830a7f7777cbdef6
Nitesh Goyal <ish.nitesh.goyal2060@gmail.com>
2025-10-13 15:49:04 -0400

    Added Hilt, Hilt navigation compose, Ksp

---
+++ app/src/main/java/com/shutterfly/imagecanvas/CanvasApplication.kt
@@ -0,0 +1,8 @@
+package com.shutterfly.imagecanvas
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CanvasApplication: Application() {
+}
--- app/src/main/java/com/shutterfly/imagecanvas/MainActivity.kt
+++ app/src/main/java/com/shutterfly/imagecanvas/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.shutterfly.imagecanvas.ui.theme.ImageCanvasTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
